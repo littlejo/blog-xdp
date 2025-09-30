b095883e4d3fbed7bd745accb1b0e2137a032e73
Joseph Ligier <joseph.ligier@accenture.com>
2025-09-30 23:58:27 +0200

    first commit

---
+++ src/helper.rs
@@ -0,0 +1,45 @@
+use aya_ebpf::programs::XdpContext;
+use aya_ebpf_bindings::helpers::bpf_csum_diff;
+use core::mem::size_of;
+use network_types::ip::Ipv4Hdr;
+
+#[inline(always)]
+pub fn csum_fold(mut csum: u64) -> u16 {
+    for _i in 0..4 {
+        if (csum >> 16) > 0 {
+            csum = (csum & 0xffff) + (csum >> 16);
+        }
+    }
+    !(csum as u16)
+}
+
+#[inline(always)]
+pub fn iph_csum(iph: *mut Ipv4Hdr) -> u16 {
+    unsafe {
+        (*iph).check = [0, 0];
+    }
+
+    let csum: u64 =
+        unsafe { bpf_csum_diff(0 as *mut u32, 0, iph as *mut u32, Ipv4Hdr::LEN as u32, 0) } as u64;
+
+    csum_fold(csum).to_be()
+}
+
+#[inline(always)]
+pub fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, u16> {
+    let start = ctx.data();
+    let end = ctx.data_end();
+    let len = size_of::<T>();
+
+    if start + offset + len > end {
+        return Err(1);
+    }
+
+    Ok((start + offset) as *const T)
+}
+
+#[inline(always)]
+pub fn ptr_at_mut<T>(ctx: &XdpContext, offset: usize) -> Result<*mut T, u16> {
+    let ptr = ptr_at::<T>(ctx, offset)?;
+    Ok(ptr as *mut T)
+}
