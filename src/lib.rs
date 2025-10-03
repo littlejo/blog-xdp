2d3058e14ef20eb5b012a352e3c696d06ac0bba3
Joseph Ligier <joseph.ligier@accenture.com>
2025-10-03 17:49:04 +0200

    Fix generic log

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 #![no_std]
 
 pub mod helper;
+pub mod log;
--- src/log.rs
+++ src/log.rs
@@ -1,25 +1,40 @@
-trait DisplayHdr {
+use aya_log_ebpf::info;
+use aya_ebpf::programs::XdpContext;
+use network_types::{icmp::IcmpHdr, ip::Ipv4Hdr, eth::EthHdr, udp::UdpHdr};
+
+pub trait DisplayHdr {
     fn log(&self, ctx: &XdpContext);
 }
 
 impl DisplayHdr for EthHdr {
     fn log(&self, ctx: &XdpContext) {
-        info!(ctx, "EthHdr: src: {:mac} => dst: {:mac} ({})",
-              self.src_addr, self.dst_addr, self.ether_type);
+        info!(
+            ctx,
+            "EthHdr: src: {:mac} => dst: {:mac} ({})",
+            self.src_addr,
+            self.dst_addr,
+            self.ether_type
+        );
     }
 }
 
 impl DisplayHdr for Ipv4Hdr {
     fn log(&self, ctx: &XdpContext) {
-        info!(ctx, "Ipv4Hdr: src: {:i} => dst: {:i} ({})",
-              self.src_addr, self.dst_addr, self.proto as u8);
+        info!(
+            ctx,
+            "Ipv4Hdr: src: {:i} => dst: {:i} ({})", self.src_addr, self.dst_addr, self.proto as u8
+        );
     }
 }
 
 impl DisplayHdr for UdpHdr {
     fn log(&self, ctx: &XdpContext) {
-        info!(ctx, "UdpHdr: src: {} => dst: {}",
-              self.src_port(), self.dst_port());
+        info!(
+            ctx,
+            "UdpHdr: src: {} => dst: {}",
+            self.src_port(),
+            self.dst_port()
+        );
     }
 }
 
