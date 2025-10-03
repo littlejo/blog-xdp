e4d7773984cfaec9cfb81807d86f9a6f0c067387
Joseph Ligier <joseph.ligier@accenture.com>
2025-10-03 17:55:58 +0200

    Add action

---
+++ src/action.rs
@@ -0,0 +1,50 @@
+use network_types::{icmp::IcmpHdr, ip::Ipv4Hdr, eth::EthHdr, udp::UdpHdr};
+
+pub enum Mode {
+    Src,
+    Dst,
+}
+
+pub trait HasAddr<A> {
+    fn addr(&self, mode: Mode) -> A;
+}
+
+impl HasAddr<[u8; 6]> for EthHdr {
+    #[inline(always)]
+    fn addr(&self, mode: Mode) -> [u8; 6] {
+        match mode {
+            Mode::Src => self.src_addr,
+            Mode::Dst => self.dst_addr,
+        }
+    }
+}
+
+impl HasAddr<[u8; 4]> for Ipv4Hdr {
+    #[inline(always)]
+    fn addr(&self, mode: Mode) -> [u8; 4] {
+        match mode {
+            Mode::Src => self.src_addr,
+            Mode::Dst => self.dst_addr,
+        }
+    }
+}
+
+impl HasAddr<u16> for UdpHdr {
+    #[inline(always)]
+    fn addr(&self, mode: Mode) -> u16 {
+        match mode {
+            Mode::Src => self.src_port(),
+            Mode::Dst => self.dst_port(),
+        }
+    }
+}
+
+#[inline(always)]
+pub fn drop<H, A>(hdr: *const H, needle: A, mode: Mode) -> bool
+where
+    H: HasAddr<A>,
+    A: PartialEq + Copy,
+{
+    let addr = unsafe { (*hdr).addr(mode) };
+    addr == needle
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![no_std]
 
 pub mod helper;
+pub mod action;
 pub mod log;
